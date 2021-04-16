b87fd517ffac96820fdad40cc71f85f0f3123283
sanghyeonchoi <76832243+sanghyeonchoi@users.noreply.github.com>
2021-04-16 23:27:54 +0900

    forEach 배열

---
+++ array/src/array/arrayTest.java
@@ -0,0 +1,17 @@
+package array;
+
+public class arrayTest {
+
+	public static void main(String[] args) {
+		int [] array = {1,2,3,4,5,6,7,8,9,10};
+		
+		for(int value : array) {
+			System.out.println(value);
+		}
+		
+		
+				
+
+	}
+
+}
